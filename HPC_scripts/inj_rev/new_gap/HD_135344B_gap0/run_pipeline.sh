a93d7dd818ee739e22dd372888d80997d4cbf860
VWelke <vanessang20030914@gmail.com>
2026-06-18 20:31:09 +1000

    Attempt to define new gap

--- HPC_scripts/inj_rev/new_gap/HD_135344B_gap0/run_pipeline.sh
+++ HPC_scripts/inj_rev/new_gap/HD_135344B_gap0/run_pipeline.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 WORKDIR="/nexus/posix0/MIA-astro-env/myben/vawelke/inj_rev/new_gap/HD_135344B_gap0"
 VENV="/nexus/posix0/MIA-astro-env/myben/vawelke/venvs/frank_env/bin/activate"
